f97310f0de43d084b25a5667ebb71bd4338dfd76
LeoNarvaez2004 <leonardo_8.8@live.com>
2025-01-13 11:10:30 -0500

    Prueba Parcial II

---
+++ Segundo Parcial/Pruebas/Narvaez-Prueba.js
@@ -0,0 +1,20 @@
+/*
+Instrucciones: 
+1. Objetivo: Diseñar una solución modular y escalable utilizando JavaScript para gestionar el inventario 
+y las ventas de una tienda ficticia. Aplicar técnicas avanzadas de programación orientada a objetos y 
+manipulación de datos. 
+2. Duración: 90 minutos. 
+3. Formato de entrega: Subir el código al repositorio           GitHub, funcional, ya que se evaluará el codigo 
+subido yfuncional,  no se evaluará intentos. 
+4. Requisitos técnicos: 
+o Aplicación de estructuras avanzadas de datos (objetos anidados, arreglos de objetos). 
+o Uso de estructuras de control (if, switch, for, while). 
+o Implementación de funciones complejas (funciones constructoras) y funciones 
+autoejecutables. 
+o Uso de métodos get y set en Objetos. 
+o Uso de Constructores(Object, Object.create, etc); 
+o Manipulación y filtrado de datos con métodos avanzados de arreglos (map, filter, reduce, 
+sort, etc). 
+o Creación de clases con encapsulación, herencia y métodos privados. 
+*/ 
+
